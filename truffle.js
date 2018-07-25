b1a6c92c71cd4d35f1d4143663fe3b52ddd5132b
Andre Meyer <andre.meyer93@gmail.com>
2018-07-25 11:25:47 +0200

    add deploy

--- truffle.js
+++ truffle.js
@@ -1,4 +1,5 @@
-//const secret = require('./secret.json')
+const secret = require('./secret.json')
+const HDWalletProvider = require('truffle-hdwallet-provider')
 
 module.exports = {
   networks: {
@@ -6,6 +7,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    }
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(secret.mnemonic, "https://rinkeby.infura.io/");
+      },
+      network_id: '4',
+    },
   },
 };
