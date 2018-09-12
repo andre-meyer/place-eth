c93352bdc59f9aad69d708d20f14b609d391b5bb
Andre Meyer <andre.meyer93@gmail.com>
2018-09-12 16:16:16 +0200

    remove logging

--- src/components/PlaceETH/utils/index.js
+++ src/components/PlaceETH/utils/index.js
@@ -58,7 +58,7 @@ export const collectChangedPixelBoundaries = (drawSpace, changeLogKeyBoundaries,
       ).reverse().join('') // right aligned in binary
 
       const changeCountBoundary = chunk.changes[boundaryIndex] || 0
-      console.log({ changeCountBoundary })
+      
       const pixelsChangedInBoundary = 64 
       const priceForBoundary = pixelsChangedInBoundary * BASE_COST * PRICE_CLIMB[changeCountBoundary]
 
